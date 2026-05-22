2d5774a3eaac6823aebaf67dd6fcfadc3f559db1
Aditya Raj <cyberadi336@gmail.com>
2026-05-22 20:52:33 +0530

    feat : put proper prisma instantiation

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,24 +1,10 @@
-import { PrismaPg } from "@prisma/adapter-pg"
-import { PrismaClient } from "@/generated/prisma/client"
+import "dotenv/config";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../generated/prisma/client";
 
-const connectionString = process.env.DATABASE_URL
+const connectionString = `${process.env.DATABASE_URL}`;
 
-if (!connectionString) {
-  throw new Error("DATABASE_URL is not defined")
-}
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
 
-const adapter = new PrismaPg({
-  connectionString,
-})
-
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
-}
-
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({ adapter })
-
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma
-}
+export { prisma };
